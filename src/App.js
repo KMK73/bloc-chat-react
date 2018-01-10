8281b14859b4acee736c7ad288d66447bb3296fe
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2018-01-10 00:27:49 -0500

    assignment 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAPhwa6eZ5-KtntpjcR6r5o-W4xwW90giU",
+  authDomain: "bloc-chat-6e7d4.firebaseapp.com",
+  databaseURL: "https://bloc-chat-6e7d4.firebaseio.com",
+  projectId: "bloc-chat-6e7d4",
+  storageBucket: "bloc-chat-6e7d4.appspot.com",
+  messagingSenderId: "470964481768"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+    constructor(props){
+      super(props);
+
+      this.state = {
+        rooms: []
+      };
+
+      // reference to the rooms path in database
+      this.roomsRef = this.props.firebase.database().ref('rooms');
+
+    }
+
+    componentDidMount() {
+      // child added event is available on any Firebase reference,
+      //and it's called once for each object that has been added to the path
+      this.roomsRef.on('child_added', (snapshot) => {
+          //The snapshot includes additional metadata about the database entry as well as the actual data.
+          console.log(snapshot);
+
+          /*
+          Store the snapshot's key (snapshot.key) along with the rest of the room data.
+          When rendering an array in React,
+          each item should have a unique "key" prop, and the snapshot's key is ideal for that purpose. */
+          // const room = snapshot.val();
+          // room.key = snapshot.key;
+          // this.setState({ rooms: this.state.rooms.concat( room ) })
+
+
+          let key = snapshot.key.toString();
+          const room = {
+            id: snapshot.key,
+            key : snapshot.val()
+          }
+          // add the snapshots data with .val to the state rooms
+          // .concat either adds items to an arrary or merges and returns a new array
+          this.setState({
+              rooms: this.state.rooms.concat( room )
+          })
+        });
+    }
+
+    render() {
+        return (
+          <div className="">
+            {
+              this.state.rooms.map(
+                (room, index) =>
+                <p>{room.key}</p>
+
+            )}
+          </div>
+        )
+    }
+}
+
+export default RoomList
